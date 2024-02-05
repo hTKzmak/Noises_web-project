13312c72fd01ff2c3d8162629c9d88e8f2f79b66
hTKzmak <ineedrtx@gmail.com>
2024-02-05 15:41:55 +0300

    adding frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import InDevelopment from './components/InDevelopment';
+
+function App() {
+  return (
+    <div className="App">
+      <InDevelopment/>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/InDevelopment.jsx
@@ -0,0 +1,11 @@
+import imgSrc from '../media/Logo.png';
+
+function InDevelopment() {
+    return (
+        <div className='in-dev'>
+            <img src={imgSrc} alt="Logo" />
+        </div>
+    )
+}
+
+export default InDevelopment
