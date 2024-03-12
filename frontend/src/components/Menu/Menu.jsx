409b1a239aef4e4c692ed2948eef1236a31980fa
hTKzmak <ineedrtx@gmail.com>
2024-03-12 23:09:33 +0300

    adding mobile menu

--- frontend/src/components/Menu/Menu.jsx
+++ frontend/src/components/Menu/Menu.jsx
@@ -1,6 +1,10 @@
 import { Link } from 'react-router-dom'
 import style from './Menu.module.css'
 import { ReactComponent as Logo } from './media/Noises_logo.svg'
+import { ReactComponent as Home } from './media/icons/home.svg'
+import { ReactComponent as Albums } from './media/icons/albums.svg'
+import { ReactComponent as Performers } from './media/icons/performers.svg'
+import { ReactComponent as Genres } from './media/icons/genres.svg'
 
 function Menu() {
     return (
@@ -10,7 +14,7 @@ function Menu() {
                     <Logo />
                 </Link>
 
-                <div className={style.browseMusicList}>
+                <div className={style.browseMusic}>
                     <h2>Browse music</h2>
                     <ul>
                         <Link to={'/'}>
@@ -27,6 +31,21 @@ function Menu() {
                         </Link>
                     </ul>
                 </div>
+            </div>
+
+            <div className={style.browseMusicMobile}>
+                    <Link to={'/'}>
+                        <Home/>
+                    </Link>
+                    <Link to={'/albums'}>
+                        <Albums/>
+                    </Link>
+                    <Link to={'/performers'}>
+                        <Performers/>
+                    </Link>
+                    <Link to={'/genres'}>
+                        <Genres/>
+                    </Link>
             </div>
         </div>
     )
