6f0b0daf238be3c507ee5f73b9139c09fecf1184
hTKzmak <ineedrtx@gmail.com>
2024-03-25 13:01:53 +0300

    little update

--- frontend/src/components/BlocksList/items/MainBlock/index.jsx
+++ frontend/src/components/BlocksList/items/MainBlock/index.jsx
@@ -13,7 +13,7 @@ function Block() {
 
     // функция по изменению заднего фона
     function randomBackground() {
-        const array = ["https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExcnY5cm43aWRya3B2eWRibjQzZWNjOXluYWd0cW41b3FtamZoZ2R6MCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/TqkuOoKtmYI80/giphy.gif",
+        const array = ["https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExOGgwenp2cm9udGhzMGFrbzEwaXJlOWxlZTNodnl4eHV6dnU0cXdnYiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/2A4E5BJitSAsL8fYGi/giphy.gif",
             "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExeWR1ZWljc3Z4Y2sza3Y3cDl1bXU5NG5lZHRjaGR1dDJhZTRicWYxMCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/SKVrls59hoqL861ezx/giphy.gif",
             "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExeGdndHM0dnl6YnY4b2dzdG4zNHl0ZnR5Znp6anhlMjZ4bzBxajdnciZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/tObuxzmlzUM0kC1K05/giphy-downsized-large.gif",
             "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExN2s2MThqOHR1dDVzYWIwc2J4bDdhYWRrMWs5d3V0bzd0eXBvc3d1ZiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/Sdlh2SCTiASAgtwyyU/giphy.gif"];
